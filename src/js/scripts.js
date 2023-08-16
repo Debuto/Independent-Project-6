b7a3d9fb154668821cffcddc507088690ff6bf19
Deante Cacatian <dwdeante0726@gmail.com>
2023-08-16 13:21:01 -0700

    Adding updated logic for catching errors if currency doesn't exist.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,7 @@ document.getElementById('conversion-form').addEventListener('submit', async func
   event.preventDefault();
 
   const usdAmount = parseFloat(document.getElementById('usd-amount').value);
+  const targetCurrency = document.getElementById('target-currency').value;
 
   if (isNaN(usdAmount)) {
     document.getElementById('conversion-result').textContent = 'Please enter a valid USD amount.';
@@ -9,19 +10,37 @@ document.getElementById('conversion-form').addEventListener('submit', async func
   }
 
   const apiUrl = `https://v6.exchangerate-api.com/v6/93892423c8a0950320cc7780/latest/USD`;
-  const response = await fetch(apiUrl);
-  const data = await response.json();
+  
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      const errorData = await response.json();
+      const errorMessage = errorData.error_message || 'An error occurred during API call.';
+      document.getElementById('error-notification').textContent = `Error: ${errorMessage}`;
+      return;
+    }
+    
+    const data = await response.json();
 
-  const conversionRates = data.conversion_rates;
-  const conversionResult = {};
+    const conversionRates = data.conversion_rates;
 
-  for (const currency in conversionRates) {
-    conversionResult[currency] = (usdAmount * conversionRates[currency]).toFixed(2);
+    if (!conversionRates[targetCurrency]) {
+      document.getElementById('error-notification').textContent = `Error: Currency '${targetCurrency}' not found in conversion rates.`;
+      document.getElementById('conversion-result').textContent = ''; 
+      return;
+    }
+
+    const conversionResult = {};
+
+    for (const currency in conversionRates) {
+      conversionResult[currency] = (usdAmount * conversionRates[currency]).toFixed(2);
+    }
+
+    const resultHTML = `<p>${targetCurrency}: ${conversionResult[targetCurrency]}</p>`;
+
+    document.getElementById('conversion-result').innerHTML = resultHTML;
+    document.getElementById('error-notification').textContent = '';
+  } catch (error) {
+    document.getElementById('error-notification').textContent = 'An error occurred while fetching data.';
   }
-
-  const resultHTML = Object.entries(conversionResult)
-    .map(([currency, amount]) => `<p>${currency}: ${amount}</p>`)
-    .join('');
-
-  document.getElementById('conversion-result').innerHTML = resultHTML;
 });
