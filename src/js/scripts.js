c6b4300e73fc0e0bbe761abebbe4394963e5fa02
Debo <dwdeante0726@gmail.com>
2023-08-16 21:35:50 -0700

    Updating API in line 6 to call API Key from .env file

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,8 +2,8 @@
 
 // Function to perform the currency conversion
 async function performConversion(usdAmount, targetCurrency) {
-  const apiKey = '93892423c8a0950320cc7780';
-  const apiUrl = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`;
+  
+  const apiUrl = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
 
   try {
     const response = await fetch(apiUrl);
